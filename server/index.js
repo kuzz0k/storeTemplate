8c88a1f64550d7e6e575bfbd76920959327b9055
kuzz0k <kuznecov00900@yandex.ru>
2024-12-06 17:06:02 +0300

    create routes and controllers

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000
 
@@ -10,11 +12,13 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: 'working'})
-})
 
+
+
+
+app.use(errorHandler)
 
 const start = async () => {
   try {
