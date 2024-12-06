c61ad613297a64dc15c23476131a286015dedf82
kuzz0k <kuznecov00900@yandex.ru>
2024-12-06 16:01:03 +0300

    create db models

--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'working'})
+})
+
+
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+
+start()
