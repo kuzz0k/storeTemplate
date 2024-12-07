27f817cc21a575fa7671ee798089d80a6b9c72c2
kuzz0k <kuznecov00900@yandex.ru>
2024-12-07 12:09:58 +0300

    init client

---
+++ client/src/components/AppRouter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+const AppRouter = () => {
+  return (
+    <div>AppRouter</div>
+  )
+}
+export default AppRouter
