fe23be1e272f5a6f95f425150878db082eef3d4a
kuzz0k <kuznecov00900@yandex.ru>
2024-12-08 20:58:51 +0300

    device page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,19 @@
 import React, { useContext } from 'react'
 import { Context } from '../index'
 import { observer } from 'mobx-react-lite'
+import {useNavigate} from 'react-router-dom'
+import { ADMIN_ROUTE, LOGIN_ROUTE } from '../utils/consts'
  
 const NavBar = observer(() => {
+  const history = useNavigate()
   const {user} = useContext(Context)
   return (
     <nav>
       <button>Главная</button>
       {user.isAuth ?
         <div>
-          <button>Войти</button>
-          <button>Админ пакель</button>
+          <button onClick={() => history(ADMIN_ROUTE)}>Админ панель</button>
+          <button onClick={() => history(LOGIN_ROUTE)}>Выйти</button>
         </div>
       :
         <div>
--- client/src/pages/DevicePage.jsx
+++ client/src/pages/DevicePage.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import star from '../assets/star.png'
+
 
 const DevicePage = () => {
+  const device = {id: 1, name: 'product1', rating: 5, price: 1000, img: ''}
+  const description = []
   return (
-    <div>DevicePage</div>
+    <div>
+      <div className='Row'>
+        <img src={device.img} alt="" />
+        <div>
+          <h2>{device.name}</h2>
+        </div>
+        <div
+        style={{background: `url(${star}) no-repeat center center`, width:240, height: 240, backgroundSize: 'cover', fontSize:64}}
+        >
+          {device.rating}
+        </div>
+        <div>
+          <h3>{device.price}</h3>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+      <div className="description">
+        <h2>Характеристики</h2>
+        {description.map(info =>
+          <div key={info.id}>
+            {info.title}: {info.description}
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
